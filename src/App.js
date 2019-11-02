49cce25cadc5366466b86ff5dd3ce350bc0ed1a7
Nicola <me@nicolacolliva.com>
2019-11-02 15:42:14 +0100

    up

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Ecology System World
-        </h1>
-        <h2>
-          Site under construction
-        </h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
