0857c6ee86195cf4fac1fa774c161608cabed27f
Nicola <me@nicolacolliva.com>
2019-11-02 16:29:46 +0100

    Updates

---
+++ precache-manifest.4bad5abe44457617244ac4ebe820e970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e98df1b7f188cbdb33e4852b7f9fb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7df988fc9f31e34b7e1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0a8dcb7556d468bb3d24",
+    "url": "/static/js/2.b4c39b1d.chunk.js"
+  },
+  {
+    "revision": "b7df988fc9f31e34b7e1",
+    "url": "/static/js/main.82dca5df.chunk.js"
+  },
+  {
+    "revision": "5b16b73c2cda93707c0f",
+    "url": "/static/js/runtime-main.5276e763.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
