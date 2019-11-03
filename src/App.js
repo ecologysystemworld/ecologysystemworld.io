34df9a52b47446bf39ca16fd4ff1b98a80cd3645
sginestrini <s.ginestrini@gmail.com>
2019-11-03 15:24:17 +0100

    Update index.html and App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ESW- 100% biodegradable is coming!</p>
-        moreno@ecologysystemworld.io Website Construction in progress
+        <h1>ESW- 100% biodegradable is coming!</h1>
+        <h3>Website Construction in progress</h3>
+        <h4>moreno@ecologysystemworld.io</h4>
       </header>
     </div>
   );
