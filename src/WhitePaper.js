c84420ff807bd51b3570dc343b1de1f1899f9792
Nicola <nicola.colliva@betnow.io>
2020-02-08 18:27:48 +0100

    up

--- src/WhitePaper.js
+++ src/WhitePaper.js
@@ -1,9 +1,12 @@
 // --- Post bootstrap -----
+import throttle from 'lodash.throttle';
 import React from 'react';
 import {Page} from 'react-pdf';
 import {Document} from 'react-pdf/dist/entry.webpack';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 import whitePaper from 'static/PDF/Brochure.pdf';
 import AppAppBar from './modules/views/AppAppBar';
+import AppFooter from './modules/views/AppFooter';
 import withRoot from './modules/withRoot';
 
 const options = {
@@ -13,10 +16,22 @@ const options = {
 };
 
 class WhitePaper extends React.Component {
-  state = {
-    file: whitePaper,
-    numPages: null,
-    pageNumber: 1,
+  constructor(props) {
+    super(props);
+    this.state = {width: null, file: whitePaper, numPages: null, pageNumber: 1};
+  }
+
+  componentDidMount() {
+    this.setDivSize();
+    window.addEventListener('resize', throttle(this.setDivSize, 500));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', throttle(this.setDivSize, 500));
+  }
+
+  setDivSize = () => {
+    this.setState({width: this.pdfWrapper.getBoundingClientRect().width});
   };
 
   onDocumentLoadSuccess = ({numPages}) => {
@@ -28,21 +43,35 @@ class WhitePaper extends React.Component {
     return (
       <React.Fragment>
         <AppAppBar />
-        <div className="Example">
-          <div className="Example__container">
-            <div className="Example__container__document">
-              <Document
-                file={file}
-                onLoadSuccess={this.onDocumentLoadSuccess}
-                options={options}
-              >
-                {Array.from(new Array(numPages), (el, index) => (
-                  <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                ))}
-              </Document>
-            </div>
+        <div
+          id="row"
+          style={{
+            width: '100vw',
+            display: 'flex',
+            overflow: 'hidden',
+          }}
+        >
+          <div
+            id="pdfWrapper"
+            style={{width: '90vw'}}
+            ref={ref => (this.pdfWrapper = ref)}
+          >
+            <Document
+              file={file}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+              options={options}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  width={this.state.width}
+                />
+              ))}
+            </Document>
           </div>
         </div>
+        <AppFooter />
       </React.Fragment>
     );
   }
