9536af0b5781f954f7ba2ebd60f4b4ebfe3971ae
Nicola <nicola.colliva@betnow.io>
2020-05-07 23:34:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from 'Home';
 import NoMatch from 'NoMatch';
 import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import WhitePaper from 'WhitePaper';
 import './App.css';
 import logo from './logo.svg';
@@ -11,23 +11,20 @@ class App extends React.Component {
     const history = this.props.hist;
     return (
       <HashRouter history={history} basename={process.env.PUBLIC_URL}>
-        <Route
-          render={({location}) => (
-            <Switch location={location}>
-              <Route exact="/home" component={Home} />
-              <Route exact="/whitePaper" component={WhitePaper} />
-              <Route component={NoMatch} />
-              <div className="App">
-                <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <h1>ESW- 100% biodegradable is coming!</h1>
-                  <h3>Website Construction in progress</h3>
-                  <h4>moreno@ecologysystemworld.io</h4>
-                </header>
-              </div>
-            </Switch>
-          )}
-        ></Route>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/whitePaper" component={WhitePaper} />
+          <Redirect exact from="/" to="/home/" />
+          <Route component={NoMatch} />
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1>ESW- 100% biodegradable is coming!</h1>
+              <h3>Website Construction in progress</h3>
+              <h4>moreno@ecologysystemworld.io</h4>
+            </header>
+          </div>
+        </Switch>
       </HashRouter>
     );
   }
